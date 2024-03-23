7a49fedc02c50d9d5291d673b72313343a90cb3c
TeddTech <ted_thompson@live.com>
2024-03-22 22:15:42 -0700

    feat(auth): 🛂 add auth js integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
-import { defineConfig } from "vite";
-import { qwikVite } from "@builder.io/qwik/optimizer";
-import { qwikCity } from "@builder.io/qwik-city/vite";
 import { builderDevTools } from "@builder.io/dev-tools/vite";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
-  return {
-    plugins: [builderDevTools(), qwikCity(), qwikVite(), tsconfigPaths()],
-  };
+	return {
+		plugins: [builderDevTools(), qwikCity(), qwikVite(), tsconfigPaths()],
+		optimizeDeps: {
+			include: ["@auth/core"],
+		},
+		dev: {
+			headers: {
+				"Cache-Control": "public, max-age=0",
+			},
+		},
+		preview: {
+			headers: {
+				"Cache-Control": "public, max-age=600",
+			},
+		},
+	};
 });
